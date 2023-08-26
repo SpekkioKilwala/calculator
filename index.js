29ada27ab83b6f40dc996c5c28c5b12373206345
spekkio <24884679+SpekkioKilwala@users.noreply.github.com>
2023-08-26 13:14:42 +1000

    Add basic operator functions

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 "use strict";
 // a = 2;
 // alert("JS working");
+
+
+function add(a, b) {
+    return (+a + +b);
+}
+
+function subtract(a, b) {
+    // ok hang on why do we even need a subtract?
+    // because it makes the operations clearer to the user.
+    // you can always do a "minus this number" button, or just ask for "(0) - input ="
+    return (a - b);
+}
+
+function multiply(a, b) {
+    return (a * b);
+}
+
+function divide(a, b) {
+    return (a / b);
+}
+
+function operate(operand, a, b) {
+    return operand(a, b);
+}
